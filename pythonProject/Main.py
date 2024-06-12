5cdaf1ca9574032d3270c13b9b84d7d52008dabb
yassi <xy.999@web.de>
2024-06-12 12:17:06 +0200

    Input Ressourcenvektor

--- pythonProject/Main.py
+++ pythonProject/Main.py
@@ -1,3 +1,13 @@
-# Nur kommentar bres, wir ficken das ding
-print("Hello World")
+def input_Ressourcenvektor():
+    "Wenn der Benutzer selbst den Ressourcenvektor eingeben will, muss gefragt werden wie viele Klassen der Benutzer haben will"
+    anzahl = int(input("Wie viele Ressourcenklassen haben sie: "))  # Fragt den Benutzer nach der Anzahl der Ressourcenklassen.
+    Ressourcenvektor = []  # Erstellt eine leere Liste für den Ressourcenvektor.
+
+    # Eingabe der Menge der jeweiligen Ressourcenklassen
+    for i in range(anzahl):  # Iteriert über die Anzahl der Ressourcenklassen.
+        ressource = int(input(f"Geben sie nun die Menge der Ressourcenklasse {i+1}: "))  # Fragt nach der Menge der aktuellen Ressourcenklasse.
+        Ressourcenvektor.append(ressource)  # Fügt die Menge der aktuellen Ressourcenklasse zur Liste hinzu.
+
+    return Ressourcenvektor  # Gibt den Ressourcenvektor zurück.
+
 
